a1b1da559905174e34b6ce7fb3d1c9295d69bc0b
Carlos Meneses <kenshinmc23@gmail.com>
2020-05-02 18:58:59 -0400

    class one

---
+++ src/main/java/Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction {
+
+    public static void main(String[] args){
+        for (String param: args){
+            System.out.println("parametro: "+param);
+        }
+    }
+}
