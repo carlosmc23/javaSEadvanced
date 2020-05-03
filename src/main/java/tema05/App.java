a3704ec73ae6e5402c2ec46da711f896a3c086e3
Carlos Meneses <kenshinmc23@gmail.com>
2020-05-03 15:45:15 -0400

    learning about generics

---
+++ src/main/java/tema05/App.java
@@ -0,0 +1,39 @@
+package tema05;
+
+import tema05.Clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        /*List<String> lista =new ArrayList<>();
+        lista.add("Mitocode");
+        lista.add(String.valueOf(23));*/
+
+        //String text = (String) lista.get(0);
+        /*String text = lista.get(0);
+        //Integer number= (Integer) lista.get(1);
+        //String textNumber = String.valueOf(lista.get(1));
+        String textNumber = lista.get(1);
+
+        System.out.println(text);
+        //System.out.println(number);
+        System.out.println(textNumber);*/
+
+        /*String[] array = new String[5];
+        array[0]="Mitocode";
+        array[1]*/
+
+        Clase<String, Integer, String, Double> c = new Clase<>("Mitocode", 25, "Mito", 25.0);
+        c.mostrarTipo();
+
+        List<Clase<String,Integer,String,String>> lista = new ArrayList<>();
+        lista.add(new Clase ("Mitocode", 25, "Carlos", "test"));
+        for (Clase<String,Integer,String,String> c1: lista){
+            c1.mostrarTipo();
+        }
+
+    }
+}
